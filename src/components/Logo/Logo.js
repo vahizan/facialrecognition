31dffdbf41fb854cf560fd2136852f420ce6eb90
Vahizan <vahizanv@gmail.com>
2018-06-28 08:53:17 +0100

    Adding Logo and storing sensitive information in environment variables

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import logo from './../../face-recognition.png';
 const Logo = () => {
 	return(
-		<div className="ma4 mt0 flex">
+		<div className="ma4 mt0 flex items-center">
 			<Tilt className="Tilt br2 shadow-2 " options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-			 <div className="Tilt-inner"><img src="./../../face-recognition.png" alt="face-recognition logo" /> </div>
+			 <div className="Tilt-inner"><img className="pt3" src={logo} alt="face-recognition logo" style={{height:100,width:75}}/> </div>
 			</Tilt>
 		</div>
 	);
